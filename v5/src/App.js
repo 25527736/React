12d8b8b79dfb9e125cbb57858831433974610da3
prem <200540107038@darshan.ac.in>
2022-07-30 10:52:15 +0530

    first commit

---
+++ v5/src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import Expenseitem from './Component/Expensitem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
+
